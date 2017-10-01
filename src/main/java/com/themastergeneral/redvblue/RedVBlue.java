fecc271aed88e42c683c767c3b569052cb14a2fe
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-30 20:42:08 -0400

    Jar signed mod

--- src/main/java/com/themastergeneral/redvblue/RedVBlue.java
+++ src/main/java/com/themastergeneral/redvblue/RedVBlue.java
@@ -1,27 +1,30 @@
 package com.themastergeneral.redvblue;
 
-import org.apache.logging.log4j.Logger;
-
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import org.apache.logging.log4j.Logger;
+
 import com.themastergeneral.redvblue.client.CreativeTab;
 import com.themastergeneral.redvblue.proxy.CommonProxy;
 
-@Mod(modid = RedVBlue.MODID, name = RedVBlue.MODNAME, version = RedVBlue.VERSION, dependencies = RedVBlue.DEPENDENCIES, updateJSON = RedVBlue.updateJSON, acceptedMinecraftVersions = RedVBlue.acceptedMinecraftVersions)
+@Mod(modid = RedVBlue.MODID, name = RedVBlue.MODNAME, certificateFingerprint = RedVBlue.FingerPrint, version = RedVBlue.VERSION, dependencies = RedVBlue.DEPENDENCIES, updateJSON = RedVBlue.updateJSON, acceptedMinecraftVersions = RedVBlue.acceptedMinecraftVersions)
 public class RedVBlue {
 	// Important mod info, yo.
 	public static final String MODID = "redvblue";
 	public static final String MODNAME = "Red VS Blue";
-	public static final String VERSION = "2.1.1";
+	public static final String VERSION = "2.1.2";
 	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,]";
 	public static final String acceptedMinecraftVersions = "1.12.2";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/RedVSBlue.json";
+	public static final String FingerPrint = "441b509a0f58a0ef41aca8daf1be20d96287635e";
 
 	// Initialize the creative tab for the mod
 	public static final CreativeTab creativeTab = new CreativeTab();
@@ -31,8 +34,7 @@ public class RedVBlue {
 	public static RedVBlue instance = new RedVBlue();
 
 	// Call the proxies so that they load on the correct side.
-	@SidedProxy(clientSide = "com.themastergeneral.redvblue.proxy.ClientProxy", 
-				serverSide = "com.themastergeneral.redvblue.proxy.ServerProxy")
+	@SidedProxy(clientSide = "com.themastergeneral.redvblue.proxy.ClientProxy", serverSide = "com.themastergeneral.redvblue.proxy.ServerProxy")
 	// Load common proxy, since it loads information needed for both server and
 	// client.
 	public static CommonProxy proxy;
@@ -53,5 +55,10 @@ public class RedVBlue {
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent e) {
 		proxy.postInit(e);
+	}
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent e) {
+		FMLLog.warning("Invalid fingerprint detected for Red VS Blue! TheMasterGeneral will not support this version!");
 	}
 }
